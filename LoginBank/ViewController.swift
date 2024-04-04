b5141c6318b72a5a4af2be605b8c15c0594c1769
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-04-03 21:05:33 -0600

    Initial Commit

---
+++ LoginBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginBank
+//
+//  Created by Angel Octavio Lopez Cruz on 03/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
